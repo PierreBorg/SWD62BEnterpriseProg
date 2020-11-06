11dace3811b6d2cacc6f2f6d9e2622c982ac51aa
Pierre Borg <Pierre.Borg.c10260@mcast.edu.mt>
2020-11-06 15:18:15 +0100

    Migration - creating our first repository class

--- Solution1/ShoppingCart.Data/Context/ShoppingCartDbContext.cs
+++ Solution1/ShoppingCart.Data/Context/ShoppingCartDbContext.cs
@@ -14,5 +14,13 @@ namespace ShoppingCart.Data.Context
         public DbSet<Product> Products { get; set; }
 
         public DbSet<Category> Categories { get; set; }
+        
+        protected  override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>().Property(x => x.Id).HasDefaultValueSql("NEWID()");
+        }
+    
     }
 }
